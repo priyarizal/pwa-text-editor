00417df6e28cce4ad33f89cb67fb9590b1023bc3
priyarizal <103598087+priyarizal@users.noreply.github.com>
2022-08-22 21:56:06 -0400

    putDB to accept content and add to DB

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,7 +13,18 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+
+// export const putDb = async (content) => console.error('putDb not implemented')
+
+export const putDb = async (id, content) => {
+  console.log('PUT to the database');
+  const jateDb = await openDB('jateDB', 1);
+  const jatetext = jateDb.transaction('jate', 'readwrite');
+  const store = jatetext.objectStore('jate');
+  const request = store.put({ id: id, todo: content });
+  const result = await request;
+  console.log('🚀 - data saved to the database', result);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');
--- client/src/js/install.js
+++ client/src/js/install.js
@@ -2,10 +2,29 @@ const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+    // Storing the triggered event
+    window.deferredPrompt = event;
+
+    // this removes the hidden class from the button via the toggle property
+    butInstall.classList.toggle('hidden', false);
+});
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => {
+
+    const promptEvent = window.deferredPrompt;
+    if (!promptEvent) {
+        return;
+    }
+    // this pops up the prompt
+    promptEvent.prompt();
+    // this resets deferred prompt variable and can only be used once
+    window.deferredPrompt = null;
+    butInstall.classList.toggle('hidden', true);
+});
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {
+    window.deferredPrompt = null;
+});
